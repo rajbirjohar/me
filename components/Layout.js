56b4e76101eea128d676c0de6664f7cb43d6e24e
r-jo <rajbir@rajbir.io>
2021-07-01 20:15:11 -0700

    :cyclone: Wrapping divs in attempt to fix reset css

--- components/Layout.js
+++ components/Layout.js
@@ -3,14 +3,16 @@ import { motion } from 'framer-motion'
 
 export default function Layout({ children }) {
   return (
-    <motion.main
-      initial={{ opacity: 0, scale: 0.998 }}
-      animate={{ opacity: 1, scale: 1 }}
-      exit={{ opacity: 0, scale: 0.998 }}
-      transition={{ duration: 0.15 }}
-      layout="position"
-    >
-      <main className={styles.main}>{children}</main>
-    </motion.main>
+    <main className={styles.main}>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.998 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.998 }}
+        transition={{ duration: 0.15 }}
+        layout="position"
+      >
+        {children}
+      </motion.div>
+    </main>
   )
 }
