013dd4a820fcb1c78c038f8d303b7e5d3f9d792b
r-jo <rajbir@rajbir.io>
2021-07-01 19:08:33 -0700

    :cyclone: Removed scale for now

--- components/Layout.js
+++ components/Layout.js
@@ -4,9 +4,9 @@ import { motion } from 'framer-motion'
 export default function Layout({ children }) {
   return (
     <motion.main
-      initial={{ opacity: 0, scale: 0.998 }}
-      animate={{ opacity: 1, scale: 1 }}
-      exit={{ opacity: 0, scale: 0.998 }}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
       transition={{ duration: 0.15 }}
       layout
       className={styles.main}
