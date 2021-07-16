255f1d562f86c192fd09e34b1c4f60eb5296a96a
rajbirjohar <github@rajbir.io>
2021-07-16 13:10:51 -0700

    :nail_care: Refactor

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -3,7 +3,7 @@ import fetcher from '../lib/fetcher'
 import { useState } from 'react'
 import styles from '@/styles/dashboard.module.css'
 
-const Stat = ({ title, data, caption }) => {
+const Stat = ({ data, caption }) => {
   return (
     <a
       href="https://github.com/rajbirjohar"
@@ -11,64 +11,22 @@ const Stat = ({ title, data, caption }) => {
       rel="noopener noreferrer"
       className={styles.group}
     >
-      <div className={styles.card}>
-        <p className={styles.title}>{title}</p>
-        <p>
-          <span>{data}</span> {caption}
-        </p>
-      </div>
+      {data} {caption}
     </a>
   )
 }
 
-const Skeleton = () => {
-  return (
-    <div className={styles.skeleton}>
-      <p className={styles.dummytitle}></p>
-      <p className={styles.dummydescription}></p>
-    </div>
-  )
-}
-
 export default function GithubStats() {
-  const [loading, setLoading] = useState(false)
   const { data } = useSWR('/api/github', fetcher)
-  // <div className={styles.dashboard}>
-  //   <Skeleton />
-  //   <Skeleton />
-  //   <Skeleton />
-  //   <Skeleton />
-  // </div>
-
   return (
-    // <div className={styles.dashboard}>
-    //   <Stat
-    //     title="Github Stars"
-    //     data={[data ? data.stars : 'Loading']}
-    //     caption="stars collected."
-    //   />
-    //   <Stat
-    //     title="Github Followers"
-    //     data={[data ? data.followers : 'Loading']}
-    //     caption="followers acquired."
-    //   />
-    //   <Stat
-    //     title="Repos Starred"
-    //     data={[data ? data.starred : 'Loading']}
-    //     caption="repos saved."
-    //   />
-    //   <Stat
-    //     title="Organizations"
-    //     data={[data ? data.orgsCont : 'Loading']}
-    //     caption="orgs contributed to."
-    //   />
-    // </div>
     <p>
-      So far, I collected {data ? data.stars : '...'} stars, acquired{' '}
-      {data ? data.followers : '...'} followers, saved{' '}
-      {data ? data.starred : '...'} repos, and contributed to{' '}
-      {data ? data.orgsCont : '...'} organizations. Follow me or star one of my
-      repos and watch the numbers update.
+      So far, I collected{' '}
+      <Stat data={data ? data.stars : '...'} caption="stars" />, acquired{' '}
+      <Stat data={data ? data.followers : '...'} caption="followers" />, saved{' '}
+      <Stat data={data ? data.starred : '...'} caption="repos" />, and
+      contributed to{' '}
+      <Stat data={data ? data.orgsCont : '...'} caption="organizations" />.
+      Follow me or star one of my repos and watch the numbers update.
     </p>
   )
 }
