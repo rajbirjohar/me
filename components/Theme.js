b5b7a273366e579707e08b923a4a966df61eb9c3
rajbirjohar <github@rajbir.io>
2021-08-31 09:33:18 -0700

    :full_moon_with_face: Added theme dropdown selection

--- components/Theme.js
+++ components/Theme.js
@@ -1,76 +1,34 @@
-import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
 import { SwatchIcon } from '@/components/icons/icons'
 import styles from '@/styles/footer.module.css'
-import { motion } from 'framer-motion'
 
 const ThemeChanger = () => {
-  const [mounted, setMounted] = useState(false)
-  const { resolvedTheme, setTheme } = useTheme()
-
-  // After mounting, we have access to the theme
-  useEffect(() => setMounted(true), [])
+  const { theme, setTheme } = useTheme()
 
   return (
-    <motion.button
-      aria-label="Toggle Dark Mode"
-      type="button"
-      className={styles.button}
-      whileHover={{ scale: 1.05 }}
-      whileTap={{ scale: 0.995 }}
-      transition={{ ease: 'easeInOut', duration: 0.015 }}
-      onClick={() =>
-        setTheme(
-          resolvedTheme === 'dark'
-            ? 'sepia'
-            : resolvedTheme === 'sepia'
-            ? 'light'
-            : 'dark'
-        )
-      }
-    >
-      {mounted && (
-        <span>
-          {resolvedTheme === 'dark' ? (
-            <>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                stroke="currentColor"
-              >
-                <SwatchIcon />
-              </svg>
-              Sepia
-            </>
-          ) : resolvedTheme === 'sepia' ? (
-            <>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                stroke="currentColor"
-              >
-                <SwatchIcon />
-              </svg>
-              Light
-            </>
-          ) : (
-            <>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                stroke="currentColor"
-              >
-                <SwatchIcon />
-              </svg>
-              Dark
-            </>
-          )}
-        </span>
+    <div className={styles.selectWrapper}>
+      <SwatchIcon />
+      {theme !== undefined && (
+        <select
+          className={styles.select}
+          value={theme}
+          onChange={(e) => setTheme(e.target.value)}
+        >
+          <option className={styles.option} value="dark">
+            Dark
+          </option>
+          <option className={styles.option} value="light">
+            Light
+          </option>
+          <option className={styles.option} value="sepia">
+            Sepia
+          </option>
+          <option className={styles.option} value="system">
+            System
+          </option>
+        </select>
       )}
-    </motion.button>
+    </div>
   )
 }
 
