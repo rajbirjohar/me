0a45eebdb1e1ca1071da431dc3dbebdf67003906
rajbirjohar <rajbirjohar1@gmail.com>
2022-12-23 20:19:08 -0700

    Repush to check env variables

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
-      <Footer /> 
+      <Footer />
     </ThemeProvider>
   );
 }
