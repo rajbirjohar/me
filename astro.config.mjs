b5b8ff051328a991358e67685f8b79417ed14b97
rajbirjohar <rajbirjohar1@gmail.com>
2023-11-05 15:52:18 -0700

    added journals

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,26 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rajbir.io',
-  integrations: [sitemap()],
+  site: "https://rajbir.io",
+  integrations: [sitemap(), mdx()],
   output: "hybrid",
   adapter: vercel(),
-  experimental: {
-		viewTransitions: true
-	}
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: "nord",
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true
+    }
+  }
 });
